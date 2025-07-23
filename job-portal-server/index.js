410d3c18a174952bcbb41e2f8e2059495c6752c4
Vivek Sharma <vsharma87077@gmail.com>
2025-07-23 14:25:11 +0530

    fix bugs

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -1,3 +1,4 @@
+
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -16,12 +17,8 @@ app.get('/', (req, res) => {
   res.send('Hello Developer')
 })
 
-// Username - lakshay22dhoundiyal
-// Password - u97klyTxI0FAnbnn
-
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@job-portal.7ieojkl.mongodb.net/?retryWrites=true&w=majority&appName=job-portal`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
